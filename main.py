49deedc79e28828ff8e30193ac0379f7f606a19c
Rahik Tahmid <rahiktahmid007@gmail.com>
2024-12-15 19:22:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-from addcontact import addcontact
+import addcontact
+import Duplicatecontacts
+userinput=int(input())
+"""1.addcontact
+2.duplicate contact
+"""
+if userinput==1:
+    addcontact()
+elif userinput==2:
+    Duplicatecontacts()
+else:
+    pass
+
+
 
